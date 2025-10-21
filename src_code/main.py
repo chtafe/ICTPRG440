d431dad221afeb3247ee2451af9aba8525db5329
chtafe <caitlin.hanrahan@studytafensw.edu.au>
2025-10-21 18:36:25 +1100

    TASK4

--- src_code/main.py
+++ src_code/main.py
@@ -6,6 +6,7 @@ ICTPRG440 Assignment: Task 2 Write Code
 import geopandas as gpd
 
 FILE_PATH = r"spatial_data_original\NPWS_Lookout.shp"
+# FILE_PATH = r"spatial_data_original\_.txt"
 OUTPUT_FOLDER_PATH = r"output"
 
 def read_to_geodataframe(FilePath):
@@ -16,19 +17,20 @@ def read_to_geodataframe(FilePath):
     Parameters:
     - filepath (str): path to the vector file (e.g. .shp, .geojson etc.)
     Returns:
-    - Geodataframe (GeoDataFrame)
+    - Geodataframe (GeoDataFrame) or None 
     """
 
     # Ensure valid filepath
     if not isinstance(FilePath, str):
-        raise TypeError("ERROR: filepath is not a string")
+        raise TypeError("\n\n ERROR: filepath is not a string\n\n ")
 
     # Read shapefile to a geodataframe
     try:
         GeoDataframe = gpd.read_file(FilePath)
 
     except Exception as e:
-        print("\n read_to_geodataframe() function error \n", e)
+        print("\n\n read_to_geodataframe() function error", e, "\n\n")
+        GeoDataframe = None
 
     return GeoDataframe # ---returns geodataframe object
 
@@ -45,7 +47,7 @@ def print_geodataframe(GeoDataframe):
 
     # Ensure valid geodataframe
     if not isinstance(GeoDataframe, gpd.GeoDataFrame):
-        raise TypeError("ERROR: geodataframe is not valid")
+        raise TypeError("\n\n ERROR: geodataframe is not valid\n\n ")
 
     # Iterate through geodataframe rows one-by-one, prompting user to continue to print each row
     try:
@@ -59,7 +61,7 @@ def print_geodataframe(GeoDataframe):
                 break
 
     except Exception as e:
-        print(f"Error reading file: {e}")
+        print("\n\n print_geodataframe() function error", e, "\n\n")
 
     return None
 
@@ -85,14 +87,14 @@ def project_gcs_to_pcs(InputGDF, OutputFolderPath, ESPG=3308):
         if crs and crs.to_epsg():
             print(f"EPSG: {crs.to_epsg()} - {crs.name}")
         else:
-            raise ValueError(f"input geodataframe CRS not in EPSG format: {crs}")
+            raise ValueError(f"\n\n input geodataframe CRS not in EPSG format: {crs}\n\n ")
         
     except Exception as e:
-        print(f"Error reading file: {e}")
+        print(f"\n\n Error reading file: {e}\n\n ")
 
     # Ensure valid input ESPG code
     if not isinstance(ESPG, int):
-        raise TypeError("ERROR: espg is not an integer code") #---with less code
+        raise TypeError("\n\n ERROR: ESPG is not an integer code\n\n ") 
 
     # Transform and print the CRS of the output geodataframe
     try:
@@ -102,21 +104,20 @@ def project_gcs_to_pcs(InputGDF, OutputFolderPath, ESPG=3308):
         if crs and crs.to_epsg():
             print(f"EPSG: {crs.to_epsg()} - {crs.name}")
         else:
-            raise ValueError(f"output geodataframe CRS not in EPSG format: {crs}")
+            raise ValueError(f"\n\n output geodataframe CRS not in EPSG format: {crs}\n\n ")
         
     except Exception as e:
-        print(f"Error reading file: {e}")
+        print("\n\n project_gcs_to_pcs() function error", e, "\n\n")
 
     # Ensure valid output folder path
     if not isinstance(OutputFolderPath, str):
-        raise TypeError("ERROR: output folder path is not a string") #---with less code
+        raise TypeError("ERROR: output folder path is not a string") 
 
     # Export output geodataframe to shapefile
     OutputPath = OutputFolderPath + "\\" + "output.shp"
     OutputGDB.to_file(OutputPath, driver="ESRI Shapefile")
 
     return None
-
 
 if __name__ == "__main__":
 
